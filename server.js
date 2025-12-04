681f119f36a1f1368e02a4ba58a2dd1235e53171
maheshwari <maheshwari200328@gmail.com>
2025-12-04 09:57:42 +0530

    baseurlchange

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require('express')
-const cors=require('cors')
-const mongoose=require('mongoose')
+const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
 const app = express()
-app.use(cors())
+
+app.use(cors({
+   origin: "*",
+   methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
 app.use(express.json())
-// app.get('/test',(req,res)=>{
-//     res.send('api working')
-// });
+
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
-app.use('/api', require('./route/userRoutes'));
 
-// app.post('/api/resetpassword/test',(req,res)=>{
-//     res.json({message:"Routes Works"})
-// })
+app.use('/api', require('./route/userRoutes'))
+
 mongoose.connect(process.env.URL)
-.then(()=>console.log("Mongodb connected"))
-.catch((err)=>console.log(err))
-//app.use('/api',require('./route/userRoutes'))
+  .then(() => console.log("Mongodb connected"))
+  .catch(err => console.log(err))
 
-const PORT=process.env.PORT||5000
-app.listen(PORT,()=>console.log('server running on the port 5000'));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
