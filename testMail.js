be32a78a615f31c68f2032edebb02bb8e3ad6ff3
maheshwari <maheshwari200328@gmail.com>
2025-12-04 18:31:01 +0530

    testmail changes

--- testMail.js
+++ testMail.js
@@ -1,16 +1,18 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com", 
+    port: 465,         
+    secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false   // FIX SSL error
-  }
+//   tls: {
+//     rejectUnauthorized: false   // FIX SSL error
+//   }
 });
 
 
