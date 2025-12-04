205374274b31435f4301055c844130be30d3cb3e
maheshwari <maheshwari200328@gmail.com>
2025-12-04 14:04:25 +0530

    JWT_SECRET changes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -43,7 +43,8 @@ console.log("Password from DB:", user ? user.password : "No user found");
     if (!valid) 
         return res.status(401).json({ message: 'Invalid Password' })
     
-    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT);
+    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET
+);
    return  res.status(200).json({ message: "Login Successfully", token })
     }
     catch(error){
