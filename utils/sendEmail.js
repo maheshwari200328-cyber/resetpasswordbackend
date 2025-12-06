816c464451d7ffe796a5870f5eaa334d1f2935fd
maheshwari <maheshwari200328@gmail.com>
2025-12-06 11:22:26 +0530

    onboard added

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
   try {
     const response = await resend.emails.send({
-      from: "Acme <noreply@navi30.com>",
+      from: "Acme<onboarding@resend.dev>",
       to,
       subject,
       html,
